454c583c9d83554f9fcbefc024aab23b8a1241b5
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-04 20:47:33 +0300

    Added message controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,11 @@
+class MessagesController < ApplicationController
+  def index
+    id = rand(1..Message.count)
+    if id.nil?
+      render json: { error: 'not found' }.to_json, status: 404
+    else
+      @message = Message.find(id)
+      render json: @message
+    end
+  end
+end

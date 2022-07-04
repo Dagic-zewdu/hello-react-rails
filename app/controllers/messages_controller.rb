457686db446efc8c5b9f7cdfb48f329762657f04
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-04 21:29:08 +0300

    Added rubocop file for inspection

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class MessagesController < ApplicationController
   def index
     id = rand(1..Message.count)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   root 'messages#index'
   get '/messages/', to: 'messages#index'
